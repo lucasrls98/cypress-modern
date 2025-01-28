d4beef04b6a416fc26328a83d991be8bae2f98bc
lucasrls98 <lucasrls98@gmail.com>
2025-01-27 23:29:00 -0300

    feat: starting promises study

---
+++ cypress/e2e/fundamentals/promises.cy.js
@@ -0,0 +1,15 @@
+it('Sem testes ainda', () => {
+    
+});
+
+const getSomething = () => 10
+
+
+const system = () => {
+    console.log('Init')
+    const something = getSomething()
+    console.log(`Something is ${something}`)
+    console.log('End')
+}
+
+system()
